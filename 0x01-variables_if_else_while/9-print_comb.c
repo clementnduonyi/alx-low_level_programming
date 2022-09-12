5f950c3d199ded999a32630910a5245f6c1d9109
Clement Nduonyi <meggaclem@gmail.com>
2022-09-12 01:02:46 +0100

    Possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 	int single_digit;
 
 	for (single_digit = 0; single_digit <= 9; single_digit++)
+	{
 		putchar((single_digit % 10) + '0');
 		if (single_digit == 9)
 			continue;
 
 		putchar(',');
 		putchar(' ');
+	}
 
 	putchar('\n');
 
