0fb5fcfbcec27f71a6bc742114de0c118f3e5bd9
Clement Nduonyi <meggaclem@gmail.com>
2022-09-12 01:40:57 +0100

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 		if (base_16 <= 9)
 			putchar((base_16 % 10) + '0');
 		if (base_16 > 9)
-	        	for (base_16 ='a'; base_16 <= 'f'; base_16++)
+			for (base_16 = 'a'; base_16 <= 'f'; base_16++)
 				putchar(base_16);
 	}
 
