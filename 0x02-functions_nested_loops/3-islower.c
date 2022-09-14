b4021f100d3fb26614e641ca419aec860f174387
Clement Nduonyi <meggaclem@gmail.com>
2022-09-14 01:33:58 +0100

    checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
  * _islower - Checks for lowercase character
  *
  * Return: 1 if c is lowercase
- * 	0 if c is not lowercase
+ * 0 if c is not lowercase
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
 		
-		else
-			return (0);
+	else
+		return (0);
 }
