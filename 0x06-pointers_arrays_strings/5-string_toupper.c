7981fae10ef1c745c092fc29f05620630b0b4f51
Clement Nduonyi <meggaclem@gmail.com>
2022-09-23 23:59:19 +0100

    fixes

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,16 @@
  */
 char *string_toupper(char *c)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (c[i] != '\0')
 	{
 		if (c[i] >= 97 && c[i] <= 122)
 		{
 			c[i] = c[i] - 97 + 65;
 		}
-	}	i++;
-
+		i++;
+	}
 	return (c);
 }
