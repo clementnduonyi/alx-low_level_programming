af6c23863c85760ff5bf0ea648a47863af874c13
Clement Nduonyi <meggaclem@gmail.com>
2022-09-30 17:51:43 +0100

    returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		str_count = 1;
-		return(str_count + _strlen_recursion(s + 1));
+		return(str_count + _strlen_recursion (s + 1));
 	}
 	return (0);
 }
