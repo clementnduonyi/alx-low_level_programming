f2fce022121857440f5068016f9632ca1808224c
Clement Nduonyi <meggaclem@gmail.com>
2022-10-04 17:23:04 +0100

    returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,7 +4,7 @@
  * *_strdup - returns a pointer to a newly
  * allocated space in memory, which contains
  * a copy of the string given as a parameter.
- * @s: String pointer
+ * @str: String pointer
  *
  * Return: String characters
  */
