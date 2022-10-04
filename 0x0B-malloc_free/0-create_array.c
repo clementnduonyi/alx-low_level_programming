985b35bfc56dbf6ab629fc597ee0765b0458c93b
Clement Nduonyi <meggaclem@gmail.com>
2022-10-04 16:25:46 +0100

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *str;
-	
+
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
@@ -21,5 +21,5 @@ char *create_array(unsigned int size, char c)
 	{
 		str[i] = c;
 	}
-	 return (str);
+	return (str);
 }
