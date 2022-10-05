2a491d2e98dddfdd95644a87602370429d6ff44e
Clement Nduonyi <meggaclem@gmail.com>
2022-10-05 10:19:38 +0100

    creates an array of chars

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j = 0;
 	unsigned int concatlen;
 
-	if (s1 == NULL || s2 == NULL)
-		return (" ");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = 0;
 	while (s1[len1])
 		len1++;
