7f659f0cf3b2f9944b491e3884c7892ebc071715
Clement Nduonyi <meggaclem@gmail.com>
2022-10-11 16:45:48 +0100

    prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,31 +10,35 @@
  */
 void print_dog(struct dog *d)
 {
-	if (!d)
+	if (d)
 	{
-		return;
-	}
-	else
-	{
-		if (d->name == NULL)
+		if (d->name)
 		{
-			printf("Name: nil\n");
+			printf("Name: %s\n", d->name);
 		}
 		else
 		{
-			printf("Name: %s\n", d->name);
+			printf("Name: nil\n");
 		}
 		if (d->age)
 		{
 			printf("Age: %.6f\n", d->age);
 		}
 		else
-		{	
+		{
 			printf("Age: nil\n");
 		}
 		if (d->owner)
 		{
 			printf("Owner: %s\n", d->owner);
 		}
-	}	
+		else
+		{
+			printf("Owner: nil\n");
+		}
+	}
+	else
+	{
+		return;
+	}
 }
