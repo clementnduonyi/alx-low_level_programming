b932c53563944e46b876f82f6f16638b7b0dc457
Clement Nduonyi <meggaclem@gmail.com>
2022-10-13 10:18:27 +0100

    prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - Prints a name
+ * @name: Name to br printed
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || f)
+	{
+		f(name);
+	}
+	else
+		return;
+}
